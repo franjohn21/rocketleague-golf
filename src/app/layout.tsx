8e53e2d7f3c9e6043318a737a0b924654f248ba6
Francis John <7452924+franjohn21@users.noreply.github.com>
2025-03-09 13:39:27 -0700

    favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -63,6 +63,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <link rel="icon" href="/favicon.svg" type="image/svg+xml" />
+      </head>
       <body className={`${inter.className} bg-gray-100`}>
         {children}
       </body>
